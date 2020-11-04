84bbcef16e33ac2703c0d2a134542b9d9819539d
Gromeu2000 <45124641+Gromeu2000@users.noreply.github.com>
2020-11-04 22:30:39 +0100

    Gettting there

--- Assigments/layout/lib/main.dart
+++ Assigments/layout/lib/main.dart
@@ -10,12 +10,52 @@ class Screen1 extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
-              color: Colors.white,
-              height: 325,
-              child: Image.asset('assets/Devices.jpg'),
+              height: 400,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/Dev22.png'),
+                  scale: 1,
+                fit: BoxFit.none,
+                ),
+                color: Color.fromRGBO(192, 204, 207, 1),
+              ),
+
+             child: Row(
+               crossAxisAlignment: CrossAxisAlignment.start,
+               children: <Widget> [
+                 Container(
+                   padding: EdgeInsets.only(top: 30, left: 15),
+                    child: Icon(
+                     Icons.keyboard_backspace_outlined,
+                     color: Colors.grey[600], 
+                     size: 35
+                     )
+                 ),
+
+                  Container(
+                     child: Icon(
+                     Icons.logout,
+                     color: Colors.grey[600],
+                     size: 25,
+                    ),
+                 ),
+          
+                Container(
+                    padding: EdgeInsets.only(top: 30, right: 15),
+                     child: Icon(
+                     Icons.logout,
+                     color: Colors.grey[600],
+                     size: 25,
+                    ),
+                 ),
+               ],
+               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+             ),
             ),
+
             Expanded(
                child: Container(
                 height: 100,
