708e8ee7e78ec376a709b2b05703f20ab1418670
Gromeu2000 <45124641+Gromeu2000@users.noreply.github.com>
2020-11-05 10:57:16 +0100

    wtf

--- Assigments/layout/lib/screens/animal_screen.dart
+++ Assigments/layout/lib/screens/animal_screen.dart
@@ -18,40 +18,7 @@ class AnimalScreen extends StatelessWidget {
                 ),
                 color: Color.fromRGBO(192, 204, 207, 1),
               ),
-
-             child: Row(
-               crossAxisAlignment: CrossAxisAlignment.start,
-               children: <Widget> [
-                 Container(
-                   padding: EdgeInsets.only(top: 30, left: 15),
-                    child: Icon(
-                     Icons.keyboard_backspace_outlined,
-                     color: Colors.grey[600], 
-                     size: 35
-                     )
-                 ),
-
-                  Container(
-                     child: Icon(
-                     Icons.logout,
-                     color: Colors.grey[600],
-                     size: 25,
-                    ),
-                 ),
-          
-                Container(
-                    padding: EdgeInsets.only(top: 30, right: 15),
-                     child: Icon(
-                     Icons.logout,
-                     color: Colors.grey[600],
-                     size: 25,
-                    ),
-                 ),
-               ],
-               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-             ),
             ),
-
             Expanded(
                child: Container(
                 height: 100,
